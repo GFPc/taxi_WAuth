e64c9f989733ec832b7125c3a9f483ef27ef8c97
GFP <fealx15@gmail.com>
2024-12-06 23:22:00 +0700

    Fixing send-message for ibronevik api

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
-
+function hex(arrayBuffer)
+{
+    return Array.prototype.map.call(
+        new Uint8Array(arrayBuffer),
+        n => byteToHex[n]
+    ).join("");
+}
 function verifyKey(req, res, next) {
     const apiKey = req.headers['x-api-key'];
     console.log("RECEIVED API KEY: " + apiKey);
-    console.log("BASE64 DECODED API KEY: " + Buffer.from(apiKey, 'base64').toString('ascii'));
+    console.log("BASE64 DECODED API KEY: " + hex(Buffer.from(apiKey, 'base64')));
     const validApiKey = process.env.API_KEY;
 
     if (!apiKey || apiKey !== validApiKey) {
